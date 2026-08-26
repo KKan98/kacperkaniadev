cc78dd1ffa370b7bec240ffa4cc239fdfbb388e6
Kacper Kania <kacper.kania@atos.net>
2026-08-26 12:07:11 +0200

    feat: create starting angular project, add header component

---
+++ kacperkaniadev/src/app/header/header.ts
@@ -0,0 +1,19 @@
+import { Component, computed, OnInit, signal } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  imports: [RouterLink],
+  selector: 'app-header',
+  styleUrl: './header.css',
+  templateUrl: './header.html',
+})
+export class Header implements OnInit{
+  baseHref = signal<"/kacperkaniadev" | "">('');
+
+  ngOnInit(): void {
+    this.baseHref.set(location.hostname === "kkan98.github.io" ? "/kacperkaniadev" : "");
+
+    console.log(this.baseHref(), 'hi');
+  }
+
+}
